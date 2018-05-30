67a307aaadc4437efb1ce867a57b0402fdc44184
Engy Fawaz <engy.fawaz@student.guc.edu.eg>
2018-05-30 04:40:12 +0200

    set up db

--- back-end/api/config/index.js
+++ back-end/api/config/index.js
@@ -4,7 +4,7 @@ module.exports = {
 	SECRET: "32876qihsdh76@&#!742(*#HG&#28702y&##@^!()(&^#))jhscbd",
 	MONGO_URI:
 		process.env.NODE_ENV === "production"
-			? "mongodb://lozi97:lozilozi@ds147659.mlab.com:47659/gitforce"
-			: "mongodb://localhost:27017/office-hours",
+			? ""
+			: "mongodb://localhost:27017/JSON-CSV",
 	EMAIL_REGEX: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 };
